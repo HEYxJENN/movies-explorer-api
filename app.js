e7c48f470c36796035661b0a93756cb7dac22591
HEYxJENN <95577576+HEYxJENN@users.noreply.github.com>
2023-04-12 16:01:16 +0400

    limiter added

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// require('dotenv').config();
 const express = require('express');
+const rateLimit = require('express-rate-limit'); // limiter
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -20,6 +20,14 @@ const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect('mongodb://localhost:27017/movies', {});
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+app.use(limiter); // limiter
 app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(bodyParser.json());
